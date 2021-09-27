782deb5055f6ad49b27944086ded6dd0baabd87f
krishnasaiv <vootla.sai@gmail.com>
2021-09-27 15:33:04 +0530

    Time: 56 ms (72.51%), Space: 16.2 MB (65.97%) - LeetHub

--- 530-minimum-absolute-difference-in-bst/530-minimum-absolute-difference-in-bst.py
+++ 530-minimum-absolute-difference-in-bst/530-minimum-absolute-difference-in-bst.py
@@ -5,31 +5,64 @@
 #         self.left = left
 #         self.right = right
 
-def InOrderTraversal(root, l):
+########### Constant Space solution
+
+
+prev = minVal = float('inf')
+
+def InOrderTraversal(root):
     if root is None:
         return
+    global prev, minVal
     
-    InOrderTraversal(root.left, l)
-    l.append(root.val)
-    InOrderTraversal(root.right, l)
-    
-    
+    InOrderTraversal(root.left)
+    # print("----------------------------")
+    # print(prev, minVal)
+    minVal = min(minVal, abs(root.val-prev))
+    prev = root.val
+    # print(root.val)
+    # print(prev, minVal)
+    InOrderTraversal(root.right)
 
 class Solution:
     def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
-        inOrder_BST = list()
-        
-        InOrderTraversal(root, inOrder_BST)
-        minVal = float('inf')
-        
-        for i in range(1, len(inOrder_BST)):
-            minVal = min(minVal, inOrder_BST[i] - inOrder_BST[i-1])
-        
+        global prev, minVal
+        prev = minVal = float('inf')
+        InOrderTraversal(root)
         return(minVal)
 
 
+
+
+
+
+########### Linear Space solution
+# def InOrderTraversal(root, l):
+#     if root is None:
+#         return
+    
+#     InOrderTraversal(root.left, l)
+#     l.append(root.val)
+#     InOrderTraversal(root.right, l)
+    
+    
+
+# class Solution:
+#     def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+#         inOrder_BST = list()
+        
+#         InOrderTraversal(root, inOrder_BST)
+#         minVal = float('inf')
+        
+#         for i in range(1, len(inOrder_BST)):
+#             minVal = min(minVal, abs(inOrder_BST[i] - inOrder_BST[i-1]))
+        
+#         return(minVal)
+
+
 ############# Time Complexity: O(n) #############
-## 1. Traverse throug each node once ---> O(n)
+## 1. Traverse through each node of the BST once ---> O(n)
+## 2. Traverse through the InOrder traversal & find the min of diffrence between consequrive nodes ---> O(n)
 
 ############# Space Complexity: O(n) #############
 ## 1. Space for recursing through the tree ---> O(n) | Total recursive calls on stack would be the max depth of the tree. The worst case max-depth would be O(n) because they did not mention that the BST is balanced.
