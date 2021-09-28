20bd7ab544d858f1d64178fab0c2d96b7567d6fd
krishnasaiv <vootla.sai@gmail.com>
2021-09-28 17:11:13 +0530

    Time: 4996 ms (11.01%), Space: 15.3 MB (34.89%) - LeetHub

---
+++ 55-jump-game/55-jump-game.py
@@ -0,0 +1,22 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        if len(nums) == 1:
+            return True
+        
+        reachable = [False]*len(nums)
+        reachable[0] = True
+        
+        
+        for i in range(1, len(nums)):
+            lookBack = 1
+            
+            while i-lookBack >= 0:
+                if nums[i-lookBack] >= lookBack and reachable[i-lookBack] :
+                    reachable[i] = True
+                    break
+                
+                lookBack += 1
+            
+        return(reachable[-1])
+            
+            
