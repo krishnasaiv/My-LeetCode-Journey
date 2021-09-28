f1d20457d4e9c3695b1b0f4436f3a6ceb38a02ca
krishnasaiv <vootla.sai@gmail.com>
2021-09-28 14:57:57 +0530

    Time: 48 ms (97.01%), Space: 14.8 MB (10.70%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.py
@@ -0,0 +1,14 @@
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        from collections import Counter, defaultdict 
+        d = defaultdict(lambda : 0, dict(Counter(nums)))
+        
+        
+        
+        m1, m2 = d[1], max(d[1], d[2]*2)
+        for i in range(3, max(d.keys())+1):
+            m1, m2 = m2, max(m1 + d[i]*i,  m2)
+        
+        return(m2)
+        
+        
