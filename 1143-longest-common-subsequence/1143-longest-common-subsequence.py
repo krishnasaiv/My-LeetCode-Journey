4cd4ea93759042af2920c7142ff3e827715682b0
krishnasaiv <vootla.sai@gmail.com>
2021-10-07 17:22:46 +0530

    Time: 348 ms (94.50%), Space: 14.4 MB (89.77%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -6,19 +6,40 @@ class Solution:
         ################################################
         l1, l2 = len(text1), len(text2)
         
-        dp =[ [0 for i in range(l2+1)] for j in range(l1+1)]
-        # print(dp)
+#         dp =[ [0 for i in range(l2+1)] for j in range(l1+1)]
+#         # print(dp)
+        
+#         for i in range(1, l1+1):
+#             for j in range(1, l2+1):
+                
+#                 # print( (i, j), (text1[i-1], text2[j-1]), dp)
+#                 if text1[i-1] == text2[j-1]:
+#                     dp[i][j] = 1 + dp[i-1][j-1]
+#                 else:
+#                     dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+#         return(dp[i][j])
+    
+    
+    
+        ################################################
+        ################# Space Optimized Iterative (Tabulation)
+        ################################################
+        l1, l2 = len(text1), len(text2)
+            
+        dp_prev = [0 for i in range(l2+1)]
         
         for i in range(1, l1+1):
+            dp_cur = [0 for i in range(l2+1)]
             for j in range(1, l2+1):
-                
-                # print( (i, j), (text1[i-1], text2[j-1]), dp)
                 if text1[i-1] == text2[j-1]:
-                    dp[i][j] = 1 + dp[i-1][j-1]
+                    dp_cur[j] = 1 + dp_prev[j-1]
                 else:
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-        return(dp[i][j])
-        
+                    dp_cur[j] = max(dp_prev[j], dp_cur[j-1])
+
+            dp_prev = dp_cur
+                
+        return(dp_prev[j])
+                    
         
         
         
