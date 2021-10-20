9c18a93c337b3bfd6bbed733769b8f42aa47588d
krishnasaiv <vootla.sai@gmail.com>
2021-10-20 12:19:48 +0530

    Time: 172 ms (40.39%), Space: 15.7 MB (14.38%) - LeetHub

---
+++ 268-missing-number/268-missing-number.py
@@ -0,0 +1,6 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        from functools import reduce
+        
+        return( reduce(lambda x,y: x^y, nums + [i for i in range(len(nums)+1)]))
+        
