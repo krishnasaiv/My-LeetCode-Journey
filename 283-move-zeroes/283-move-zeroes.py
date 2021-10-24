6462f609f3e446e58cc68034291af7188b05e5b8
krishnasaiv <vootla.sai@gmail.com>
2021-10-24 14:23:21 +0530

    Time: 156 ms (59.01%), Space: 15.4 MB (27.08%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -12,6 +12,16 @@ class Solution:
                 ind += 1
         for i in range(ind, len(nums)):
             nums[i] = 0
-        
+            
+    
+#############     Strategy         #############
+## 2 pointer iteration
+## One iterator points to the position in array & the other to the position of placing the next number
+
+############# Time Complexity: O(n) #############
+## 1. Iterate once ---> O(n)
+
+############# Space Complexity: O(1) #############
+## 1. No extra Space ---> O(1)
                 
         
