4eba56557621f8ad71a9886c001b7a17b6cb22d3
krishnasaiv <vootla.sai@gmail.com>
2021-10-24 13:35:27 +0530

    Time: 120 ms (44.81%), Space: 15.7 MB (47.54%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
@@ -3,13 +3,23 @@ class Solution:
         
         if len(nums) <= 1: return(len(nums))
         
-        l = 0
-        r = 1
+#         l = 0
+#         r = 1
         
-        while True:
-            while nums[l] == nums[r]:
-                r += 1
-                if r >= len(nums):
-                    return(l+1)
-            l += 1
-            nums[l] = nums[r]
+#         while True:
+#             while nums[l] == nums[r]:
+#                 r += 1
+#                 if r >= len(nums):
+#                     return(l+1)
+#             l += 1
+#             nums[l] = nums[r]
+
+        
+        lnth = 0
+        for i in range(1, len(nums)):
+            
+            if nums[i] > nums[lnth]:
+                lnth += 1
+                nums[lnth] = nums[i]
+        
+        return(lnth+1)
