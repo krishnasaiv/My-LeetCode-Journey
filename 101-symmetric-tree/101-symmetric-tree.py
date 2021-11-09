0a9c095e26665e543dc7a30d3112f1d0cca30dde
krishnasaiv <vootla.sai@gmail.com>
2021-11-09 15:43:08 +0530

    Time: 28 ms (95.63%), Space: 14.4 MB (17.57%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.py
+++ 101-symmetric-tree/101-symmetric-tree.py
@@ -8,16 +8,22 @@ class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
         return(self.isMirrorTree(root.left, root.right) )
     
-    
-    
-    
     def isMirrorTree(self, l, r):
-        # print("---------------")
-        # print(l, r)
         if (not l) and (not r):
             return True
         elif (l and not r) or ( not l and r) or (l.val != r.val):
             return False
-        # print(l.val, r.val)
         return( self.isMirrorTree(l.left, r.right) and self.isMirrorTree(l.right, r.left) )
-        
+
+############# Time Complexity: O(n) #############
+## 1. Traverse each node of the tree
+
+############# Space Complexity: O(n) #############
+## 1. Stack space for recusrsion which is max height of the tree
+
+
+############# Comments #############
+##      Here if the tree is not balanced & like a chain( we will find it in the first step itself). 
+##      So, the more unbalanced the quicker it takes to decide symmetry
+##      We could say the worst case takes place when the tree is balanced
+##      Can we say the time & space complexities are O(log(n))
