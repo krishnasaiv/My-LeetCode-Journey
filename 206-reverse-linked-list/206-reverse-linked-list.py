98f20d4ad6d138af4565ff170af685f8314f81a4
krishnasaiv <vootla.sai@gmail.com>
2021-12-13 14:01:50 +0530

    Time: 32 ms (88.84%), Space: 15.6 MB (77.37%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.py
+++ 206-reverse-linked-list/206-reverse-linked-list.py
@@ -4,26 +4,62 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    
+    ################################################
+    ## Method 1: Stack --> O(n), O(n)
+    ################################################
+#     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         if head is None:
+#             return(None)
+        
+#         from collections import deque
+#         q = deque()
+        
+#         while head:
+#             q.append(head)
+#             itr = head.next
+#             head.next = None
+#             head = itr
+            
+        
+#         head = itr = q.pop()
+#         while len(q):
+#             itr.next = q.pop()
+#             itr = itr.next
+            
+#         return(head)
+    
+    ################################################
+    ## Method 2: 3 Pointer Iterative --> O(n), O(1)
+    ################################################
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head is None:
-            return(None)
-        
-        from collections import deque
-        q = deque()
-        
-        while head:
-            q.append(head)
-            itr = head.next
-            head.next = None
-            head = itr
+        if head is None or head.next is None:
+            return head
+
+        prev= None
+        cur = head        
+        while cur:
+            ## Save the pointer to the rest of the linkedList
+            nxt = cur.next
             
-        
-        head = itr = q.pop()
-        while len(q):
-            itr.next = q.pop()
-            itr = itr.next
+            ## Move the connection from cur->nxt to cur->prev
+            cur.next = prev
             
-        return(head)
+            ## Modify prev, cur pointers
+            prev = cur
+            cur = nxt
+        
+        
+        return prev
+            
+            
+            
+    
+    
+    
+    ################################################
+    ## Method 2: Recursive --> O(n), O(1)
+    ################################################
             
             
         
