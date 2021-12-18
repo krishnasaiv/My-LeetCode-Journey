b063e2171de9781cb8cce7f0f25b7eaae90f42e8
krishnasaiv <vootla.sai@gmail.com>
2021-12-18 14:57:40 +0530

    Time: 36 ms (53.48%), Space: 14.2 MB (77.05%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
@@ -4,24 +4,45 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+#     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         
-        
-        endPointer = delPointer = head
-        for i in range(n):
-            endPointer = endPointer.next
+#         endPointer = delPointer = head
+#         for i in range(n):
+#             endPointer = endPointer.next
             
-        if endPointer is None:
-            head = head.next
-            return(head)
+#         if endPointer is None:
+#             head = head.next
+#             return(head)
 
-        while endPointer.next is not None:
-            endPointer = endPointer.next
-            delPointer = delPointer.next
+#         while endPointer.next is not None:
+#             endPointer = endPointer.next
+#             delPointer = delPointer.next
         
-        delPointer.next = delPointer.next.next
+#         delPointer.next = delPointer.next.next
         
-        return(head)
+#         return(head)
+    
+    
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        #### To remove nth item from the end, we have to pint to the n+1th item from the end
+        
+        l = r = head
+        
+        while n >= 0:
+            if r is None:
+                head = head.next
+                return(head)
+            r = r.next
+            n -= 1
+        
+        while r:
+            l = l.next
+            r = r.next
+        
+        l.next = l.next.next
+        return head
+        
+    
     
 ############# Time Complexity: O(n) #############
 ## 1. Iterate through the array ---> O(n)
