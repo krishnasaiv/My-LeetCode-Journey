6957449f3d3d119e42653dbb7ed252debbf5e3cf
krishnasaiv <vootla.sai@gmail.com>
2021-12-21 13:54:48 +0530

    Time: 32 ms (92.37%), Space: 14.2 MB (61.52%) - LeetHub

--- 61-rotate-list/61-rotate-list.py
+++ 61-rotate-list/61-rotate-list.py
@@ -6,6 +6,9 @@
 class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         
+        #### To rotate a linked list by k places, we have to reach k+1th node from the end
+        #### Reduce k to kRem so that 1 <= kRem <= length-1 by taking the reminder of length when divided by k
+        
         if head is None or head.next is None:
             return head
         
@@ -14,6 +17,8 @@ class Solution:
         while cur:
             length +=1
             cur = cur.next
+        
+        
         
         rotations = k % length
         if rotations > 0:
@@ -31,3 +36,10 @@ class Solution:
             head = nxt
             
         return head
+    
+############# Time Complexity: O(n) #############
+## 1. Find length ---> O(n)
+## 2. Navigate to the n+1 th node from the end ---> O(n)
+
+############# Space Complexity: O(1) #############
+## 1. No Extra space --> O(1)
