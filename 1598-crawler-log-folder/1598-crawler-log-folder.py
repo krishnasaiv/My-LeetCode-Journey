93f98688a1efd4b6794c6e39c49fec81b8628444
krishnasaiv <vootla.sai@gmail.com>
2022-02-07 18:14:11 +0530

    Time: 77 ms (26.94%), Space: 14.1 MB (98.26%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+
+        count = 0
+        
+        for i in logs:
+            if i == './':
+                continue
+            elif i == '../':
+                if count == 0:
+                    continue
+                else:
+                    count -= 1
+            else:
+                count += 1
+        
+        return max(0, count)
+        
