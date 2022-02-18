587029b8cfe9ac7aa9dc15f93fc38a04b83e5df3
krishnasaiv <vootla.sai@gmail.com>
2022-02-18 21:28:12 +0530

    Time: 139 ms (51.94%), Space: 14.7 MB (98.40%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.py
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -2,11 +2,14 @@ class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         
         m, n = len(grid), len(grid[0])
-
+        ### TRICK: We only need to maintain the dp cost values of the previous row & do not need any previous rows' costs 
+        
+        ## Populate the costs for the first row
         dp_PrevRow = grid[0]
         for j in range(1, n):
             dp_PrevRow[j] +=  dp_PrevRow[j-1]
-            
+        
+        ## Iterate from the second row
         for i in range(1, m):
             dp_CurRow = [0] * (n)
             for j in range(n):
